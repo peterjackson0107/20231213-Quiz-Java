5ee8fa35fe6f123616d678cff7fc1aea8d3265c4
通報した <peterjackson0107@gmail.com>
2024-01-09 00:51:40 +0800

    20210109-0051

---
+++ src/main/java/com/example/quiz/config/CaffeineCacheConfig.java
@@ -0,0 +1,31 @@
+package com.example.quiz.config;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+
+@EnableCaching // Spring framework 中用來驅動緩存的註解，
+//容器內只少要有一個 CacheManager 的 Bean 
+@Configuration // 配置成設定檔，並交由 Spring Boot 託管
+public class CaffeineCacheConfig {
+
+	@Bean
+	public CacheManager cacheManager() {
+		CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+		cacheManager.setCaffeine(Caffeine.newBuilder()
+// 設定過期時間: 1. 最後一次寫入或 2. 訪問後就過固定一段時間
+				.expireAfterAccess(600, TimeUnit.SECONDS)
+// 初始的緩存空間大小
+				.initialCapacity(100)
+// 緩存的最大筆數
+				.maximumSize(500));
+		return cacheManager;
+	}
+
+}
---
+++ src/main/java/com/example/quiz/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+//package com.example.quiz.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//
+//import springfox.documentation.builders.ApiInfoBuilder;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.oas.annotations.EnableOpenApi;
+//import springfox.documentation.service.ApiInfo;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//
+//@Configuration // 跟 spring 說此 class 為配置類
+//@EnableOpenApi // 通過此註釋來啟用 swagger
+//@EnableWebMvc // 引入 DelegatingWebMvcConfiguration 配置類，並啟用 spring MVC
+//public class SwaggerConfig {
+//
+//	// 接口訊息，包括 標題、說明、版本、聯絡人等
+//	private ApiInfo DEFAULT_API_INFO = new ApiInfoBuilder() //
+//			.title("Quiz Restful API") // 標題
+//			.description("Quiz API") // 說明
+////   .termsOfServiceUrl("urn:tos") // 服務條款網址，默認 urn:tos
+////   .contact(new Contact("DEFAULT", "", "")) // 聯絡人
+////   .license("Apache 2.0") //
+////   .version("v3") // 版本
+////   .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0.txt") //
+//			.build();
+//
+//	@Bean
+//	public Docket api() {
+//		// 返回文檔概要訊息
+//		return new Docket(DocumentationType.SWAGGER_2) //
+//				.apiInfo(DEFAULT_API_INFO)//
+//				.select()//
+//				.apis(RequestHandlerSelectors.basePackage("com.example.quiz.controller"))//
+//				.paths(PathSelectors.any())//
+//				.build();
+//	}
+//}
